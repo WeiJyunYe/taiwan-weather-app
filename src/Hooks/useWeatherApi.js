934ef7cab7a92ce2e86720eeb444f6097b2193f1
WeiJyunYe <weijyunye@gmail.com>
2023-01-04 17:18:22 +0800

    230104

--- src/Hooks/useWeatherApi.js
+++ src/Hooks/useWeatherApi.js
@@ -1,6 +1,5 @@
 import { useState, useEffect, useCallback } from "react"
 
-
 const fetchCurrentWeather = (locationName) => {
   return fetch(
     `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=${process.env.REACT_APP_API_KEY}&locationName=${locationName}`
@@ -12,6 +11,7 @@ const fetchCurrentWeather = (locationName) => {
       return response.json()
     })
     .then((data) => {
+      //console.log(data)
       const locationData = data.records.location[0]
       const neededData = ["WDSD", "TEMP", "HUMD"]
       const weatherElements = locationData.weatherElement.reduce(
@@ -32,7 +32,7 @@ const fetchCurrentWeather = (locationName) => {
     })
     .catch((error) => {
       console.log(error)
-      alert("系統發生錯誤，若無法正常運作，請重新整理。若仍跳出此訊息，很抱歉，暫時無法提供氣象資料。")
+      alert("系統發生錯誤，請您重新整理。若仍跳出此訊息，很抱歉，因無法取得您所在縣市氣象資料，暫時無法提供服務。")
     })
 }
 
@@ -47,6 +47,7 @@ const fetchWeatherForecast = (cityName) => {
       return response.json()
     })
     .then((data) => {
+      //console.log(data)
       const locationData = data.records.location[0]
       const neededData = ["Wx", "PoP"]
       const weatherElements = locationData.weatherElement.reduce(
@@ -66,7 +67,10 @@ const fetchWeatherForecast = (cityName) => {
         probabilityOfPrecipitation: weatherElements.PoP.parameterName,
       })
     })
-    .catch((error) => console.log(error))
+    .catch((error) => {
+      console.log(error)
+      alert("系統發生錯誤，請您重新整理。若仍跳出此訊息，很抱歉，因無法取得您所在縣市氣象資料，暫時無法提供服務。")
+    })
 }
 
 export default function useWeatherApi(currentLocation) {
